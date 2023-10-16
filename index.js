a6d807ce5ac705d6b64abaa9e4a8c638d9914de2
Akihiko Nakamura <anakamuro@earthlink.net>
2023-10-16 17:25:06 -0400

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let myObj = {
+    name: "Domenic", 
+    age: 56
+};
+
+let myObj_serialized = JSON.stringify(myObj)
+console.log(myObj_serialized);
+
+localStorage.setItem("myObj", myObj_serialized)
+
+let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+console.log(myObj_deserialized)
